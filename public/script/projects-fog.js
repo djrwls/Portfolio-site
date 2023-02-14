f74f771ff73e2f37e1a930c7ab0e2583e49524ee
Damon <120668811+djrwls@users.noreply.github.com>
2023-02-14 09:41:57 +0000

    changed projects page so it is public & only shows active projects, added archived projects

---
+++ public/script/projects-fog.js
@@ -0,0 +1,14 @@
+VANTA.FOG({
+      el: "#fog",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      highlightColor: 0xff0000,
+      midtoneColor: 0xe600ff,
+      lowlightColor: 0xffb1,
+      baseColor: 0xffffff,
+      blurFactor: 1.00,
+      speed: 5.00,
+    })
